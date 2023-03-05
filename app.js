e3d1852c22870a2f9947ae912d039602aee4ccc9
RamazanKurnaz <ramazankurnaz74@hotmail.com>
2023-03-05 22:39:01 +0300

    practice

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-function gree(name){
-    return "hello "+name 
+import axios from "axios";
+
+export async function getUsers(id){
+    const {data:users}=await axios("https://jsonplaceholder.typicode.com/users/"+id)
+    const {data:shared}=await axios("https://jsonplaceholder.typicode.com/posts/"+users.id)
+    return  console.log(users,shared) 
+
 }
-
-let greeee ="güneş"
-
-export {gree,greeee}
